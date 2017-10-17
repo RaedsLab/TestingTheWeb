c57a93b47e3f5b2c5504341f377a9b87b8715604
Raed CHAMMAM <rchammam@questel.com>
2017-10-17 14:20:04 +0200

    added start / finished folders

--- 02/src/pow.js
+++
@@ -1,7 +0,0 @@
-function pow(x, p) {
-	if(x >0 && p >0){
-			return Math.pow(x,p);
-	}else {
-		return NaN;
-	}
-}
--- 02/test/some.test.js
+++
@@ -1,3 +0,0 @@
-describe("A test suite", function() {
-   it('should fail', function() { expect(pow(2, 3)).to.equal(8); });
-});
